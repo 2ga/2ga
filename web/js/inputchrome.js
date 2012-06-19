e734a5c51d3b3e660eabe38b7ff1d44eda7f2bf9
37 <t10792fm@sfc.keio.ac.jp>
2012-06-19 11:21:56 +0900

    add inputchrome.js

---
+++ web/js/inputchrome.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Stop chrome's autocomplete from making your input fields that nasty yellow. Yuck.
+if (navigator.userAgent.toLowerCase().indexOf("chrome") >= 0) {
+	$(window).load(function(){
+		$('input:-webkit-autofill').each(function(){
+			var text = $(this).val();
+			var name = $(this).attr('name');
+			$(this).after(this.outerHTML).remove();
+			$('input[name=' + name + ']').val(text);
+		});
+	});
+}
+
