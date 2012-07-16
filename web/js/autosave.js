6dc02745c066db0e2a6ce32fb88794ad44212f48
saxsir <shohei.sasamoto@gmail.com>
2012-07-16 13:56:56 +0900

    とりあえずコミット

---
+++ web/js/autosave.js
@@ -0,0 +1,45 @@
+function saveCache(url, editor) {
+  var text;
+  if(!editor){ //undefined => true
+    text = " ";
+  }else{
+    text = editor.getValue();
+  }
+  
+  $.ajax({
+    type: 'POST',
+    url: url + ".cache",
+    data: {
+      body: text
+    },
+    success: function(data) {
+      if(!text){
+        console.log("clearcache");
+      }else{
+        console.log("savecache.");
+      }
+    }
+  });
+}
+
+function saveLocal(url, editor) {
+  if (!window.localStorage) {
+    return;
+  }
+  doSave(url, editor);
+}
+function doSave(url, editor) {
+  console.log(editor.getValue());
+  var data = {
+    text: editor.getValue()
+  }
+  window.localStorage.setItem(url, JSON.stringify(data));
+  console.log("save to local storage.");
+}
+
+function loadData(key) {
+  var data = JSON.parse(window.localStorage.getItem(key));
+  var value = data.text;
+  console.log("load from local storage.");
+  return value;
+}
